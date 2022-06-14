2a07ebd886fc43a0b0cb728671d92d98976a4cf8
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-14 14:46:42 +0900

    viewの追加

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -7,5 +7,11 @@ class NovelsController < ApplicationController
   end
 
   def create
+    Novel.create(novel_params)
+  end
+
+  private
+  def novel_params
+    params.require(:novel).permit(:title, :auther, :release_year, :genre_id, :another_area, :spot, :synopsis, :mediamix)
   end
 end
