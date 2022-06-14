e95ec93a9b8121a6d3ddfca7aef9521a28610295
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-14 19:24:23 +0900

    投稿機能実装

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -7,11 +7,16 @@ class NovelsController < ApplicationController
   end
 
   def create
-    Novel.create(novel_params)
+    @novel = Novel.new(novel_params)
+    if @novel.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   private
   def novel_params
-    params.require(:novel).permit(:title, :auther, :release_year, :genre_id, :another_area, :spot, :synopsis, :mediamix)
+    params.require(:novel).permit(:title, :auther, :release_year, :genre_id, :another_area, :spot, :synopsis, :mediamix, area_ids:[])
   end
 end
--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,10 +1,10 @@
 class Novel < ApplicationRecord
-  has_many :areas, through: :novel_areas
   has_many :novel_areas
+  has_many :areas, through: :novel_areas
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
 
-  validates :title, presence: true, :uniqueness => {:scope => :auther ,message: "その著者による当該作品はすでに投稿されています。"}
+  validates :title, presence: true
   validates :auther, presence: true
 end
