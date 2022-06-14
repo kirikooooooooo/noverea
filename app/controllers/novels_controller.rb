3c57d724f74838f04510f7cf9fe992b4206068c4
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-14 12:38:16 +0900

    ルーティング・ヘッダーリンク修正

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,4 +1,10 @@
 class NovelsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to:"novels#index"
-  get 'novels/index'
+  resources :novels, only: [:index, :new, :create]
 end
