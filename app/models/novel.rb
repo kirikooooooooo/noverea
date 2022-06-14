7508ea757f28c99697444b684cda422a3ad7c1ec
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-14 15:22:06 +0900

    フォーム　プルダウン追加

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -2,6 +2,9 @@ class Novel < ApplicationRecord
   has_many :areas, through: :novel_areas
   has_many :novel_areas
 
-  validates :title, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :title, presence: true, :uniqueness => {:scope => :auther ,message: "その著者による当該作品はすでに投稿されています。"}
   validates :auther, presence: true
 end
