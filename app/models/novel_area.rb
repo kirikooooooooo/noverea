b075b497d43e59579cb304c4c65cf30b3c2a9c15
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-16 16:27:17 +0900

    舞台の表示実装

--- app/models/novel_area.rb
+++ app/models/novel_area.rb
@@ -2,4 +2,9 @@ class NovelArea < ApplicationRecord
   belongs_to :novel
   belongs_to :area
 
+  attr_accessor(
+    :title, :auther,
+    :id, :created_at, :updated_at,
+    :country, :prefecture, :city
+   )
 end
