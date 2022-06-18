ae839a892a810b5de4459c6a53029ab41df0df94
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-18 14:28:02 +0900

    ルーティング・コントローラー・ビュー実装

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -13,11 +13,52 @@ class AreasController < ApplicationController
     end
   end
 
+  def show
+    @area = Area.find(params[:id])
+    if @area.city == nil
+      @same_country = Area.where(country:@area.country, prefecture:nil, city:nil)
+    elsif @area.city != nil
+      @same_country = Area.where(country:@area.country, prefecture:nil, city:nil)
+      @same_country_pre = Area.where(country:@area.country, prefecture:@area.prefecture, city:nil)
+    end
+    @novels = @area.novels
+    @result = []
+
+    @novels.each do |novel|
+      title = novel.title
+      auther = novel.auther
+      search_rakuten_api(title, auther)
+      @result << @item
+    end
+  end
+
   private
 
   def area_collection_params
     params
       .require(:form_area_collection)
       .permit(areas_attributes: Form::Area::REGISTRABLE_ATTRIBUTES)
+  end
+
+  def search_rakuten_api(title, auther)
+    RakutenWebService.configure do |c|
+      c.application_id = ENV["RAKUTEN_APP_KEY_ID"]
+      c.affiliate_id = ENV["RAKUTEN_AFFILIATE_KEY_ID"]
+    end
+    items = RakutenWebService::Books::Book.search(title: title, auther: auther)
+    items.first(1).each do |item|
+      @item = item
+    end
+  end
+
+  def search_rakuten_api_dvd(title)
+    RakutenWebService.configure do |c|
+      c.application_id = ENV["RAKUTEN_APP_KEY_ID"]
+      c.affiliate_id = ENV["RAKUTEN_AFFILIATE_KEY_ID"]
+    end
+    items = RakutenWebService::Books::DVD.search(title: title)
+    items.first(1).each do |item|
+      @dvditem = item
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to:"novels#index"
   get 'novels/complete'
   resources :novels, only: [:index, :new, :create, :show]
-  resources :areas, only: [:new, :create]
+  resources :areas, only: [:new, :create, :show]
 end
